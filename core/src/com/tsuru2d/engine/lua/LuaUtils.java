7052e84ac8e17f336d6ffdbc863c7985f72c8947
Andrew Sun <0xandrewsun@gmail.com>
2015-09-15 14:47:25 -0500

    Add documentation

--- core/src/com/tsuru2d/engine/lua/LuaUtils.java
+++ core/src/com/tsuru2d/engine/lua/LuaUtils.java
@@ -3,14 +3,25 @@ package com.tsuru2d.engine.lua;
 import org.luaj.vm2.*;
 
 /* package */ class LuaUtils {
-    public static Varargs bridgeJavaToLuaOut(Object javaValue) {
-        if (javaValue instanceof Varargs) {
-            return (Varargs)javaValue;
-        } else {
-            return bridgeJavaToLuaIn(javaValue);
-        }
-    }
-
+    /**
+     * Converts a Java object to its Lua equivalent. The logic
+     * is as follows:
+     * <p>
+     * <ol>
+     *     <li>{@code null} -> {@link LuaNil}</li>
+     *     <li>{@link String} -> {@link LuaString}</li>
+     *     <li>{@link Integer} -> {@link LuaInteger}</li>
+     *     <li>(other primitive wrapper classes...)</li>
+     *     <li>{@link LuaValue} -> {@link LuaValue}</li>
+     *     <li>{@link Object} -> {@link LuaUserdata}</li>
+     * </ol>
+     * <p>
+     * This method does *not* handle {@link Varargs} objects,
+     * use {@link #bridgeJavaToLuaOut(Object)} if you need to handle
+     * them. This is intended to be used when calling Lua functions
+     * from Java.
+     * @param javaValue The value to convert.
+     */
     public static LuaValue bridgeJavaToLuaIn(Object javaValue) {
         if (javaValue == null) {
             return LuaValue.NIL;
@@ -39,6 +50,45 @@ import org.luaj.vm2.*;
         }
     }
 
+    /**
+     * The same as {@link #bridgeJavaToLuaIn(Object)}, but also
+     * handles {@link Varargs} objects. This is intended to be used
+     * when returning values from a Java method to Lua.
+     * @param javaValue The value to convert.
+     */
+    public static Varargs bridgeJavaToLuaOut(Object javaValue) {
+        if (javaValue instanceof Varargs) {
+            return (Varargs)javaValue;
+        } else {
+            return bridgeJavaToLuaIn(javaValue);
+        }
+    }
+
+    /**
+     * Converts a Lua object to its Java equivalent. This method
+     * performs type checking and unboxing of primitive wrappers.
+     *
+     * <p>
+     * If {@code luaValue} is nil and {@code expectedType} does not
+     * refer to a primitive type, {@code null} will be returned.
+     *
+     * <p>
+     * If {@code expectedType} refers to a Java primitive type or its
+     * corresponding wrapper type, {@code luaValue} will be converted
+     * to the wrapper type.
+     *
+     * <p>
+     * If {@code expectedType} refers to a subclass of {@link LuaValue},
+     * the Lua object will be directly returned.
+     *
+     * <p>
+     * If {@code expectedType} refers to any other Java class,
+     * {@code luaValue} must be an instance of {@link LuaUserdata} that
+     * wraps that type.
+     *
+     * @param luaValue The value to convert.
+     * @param expectedType The Java class to convert the value to.
+     */
     public static Object bridgeLuaToJava(LuaValue luaValue, Class<?> expectedType) {
         if (luaValue.isnil() && !expectedType.isPrimitive()) {
             return null;
