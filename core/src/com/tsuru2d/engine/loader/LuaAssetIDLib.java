653ae9daed6ee3d41d65ea8dba4fbc14458482c1
Andrew Sun <0xandrewsun@gmail.com>
2015-10-07 14:39:48 -0500

    Split LuaAssetID

--- core/src/com/tsuru2d/engine/loader/LuaAssetIDLib.java
+++ core/src/com/tsuru2d/engine/loader/LuaAssetIDLib.java
@@ -1,40 +1,14 @@
 package com.tsuru2d.engine.loader;
 
 import org.luaj.vm2.LuaTable;
-import org.luaj.vm2.LuaUserdata;
 import org.luaj.vm2.LuaValue;
-import org.luaj.vm2.lib.OneArgFunction;
 import org.luaj.vm2.lib.TwoArgFunction;
 
 public class LuaAssetIDLib extends TwoArgFunction {
-    private static final LuaTable ASSETID_METATABLE;
-    static {
-        ASSETID_METATABLE = new LuaTable();
-        ASSETID_METATABLE.set(LuaValue.INDEX, new TwoArgFunction() {
-            @Override
-            public LuaValue call(LuaValue obj, LuaValue key) {
-                AssetID id = (AssetID)obj.checkuserdata();
-                return new NewAssetIDBuilder(id.getChild(key.checkjstring()));
-            }
-        });
-        ASSETID_METATABLE.set(LuaValue.TOSTRING, new OneArgFunction() {
-            @Override
-            public LuaValue call(LuaValue arg) {
-                return LuaValue.valueOf(arg.checkuserdata().toString());
-            }
-        });
-    }
-
-    private static final NewAssetIDBuilder SOUND = new NewAssetIDBuilder(AssetID.SOUND);
-    private static final NewAssetIDBuilder MUSIC = new NewAssetIDBuilder(AssetID.MUSIC);
-    private static final NewAssetIDBuilder VOICE = new NewAssetIDBuilder(AssetID.VOICE);
-    private static final NewAssetIDBuilder TEXT = new NewAssetIDBuilder(AssetID.TEXT);
-
-    public static class NewAssetIDBuilder extends LuaUserdata {
-        public NewAssetIDBuilder(AssetID id) {
-            super(id, ASSETID_METATABLE);
-        }
-    }
+    private static final LuaAssetID SOUND = new LuaAssetID(AssetID.SOUND);
+    private static final LuaAssetID MUSIC = new LuaAssetID(AssetID.MUSIC);
+    private static final LuaAssetID VOICE = new LuaAssetID(AssetID.VOICE);
+    private static final LuaAssetID TEXT = new LuaAssetID(AssetID.TEXT);
 
     @Override
     public LuaValue call(LuaValue modname, LuaValue env) {
