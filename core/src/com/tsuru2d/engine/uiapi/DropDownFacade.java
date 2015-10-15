692b644fbaa6814fff9de32ef4188ce4ac6ddf5b
NeoWu1216 <neo1216.wu@gmail.com>
2015-10-14 21:02:02 -0500

    Modify ImageButtonFacade, fix problems of DropDownFacade

--- core/src/com/tsuru2d/engine/uiapi/DropDownFacade.java
+++ core/src/com/tsuru2d/engine/uiapi/DropDownFacade.java
@@ -1,21 +1,40 @@
 package com.tsuru2d.engine.uiapi;
 
-import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Array;
 import com.tsuru2d.engine.BaseScreen;
 import com.tsuru2d.engine.lua.ExposeToLua;
+import org.luaj.vm2.LuaFunction;
 import org.luaj.vm2.LuaTable;
 
-public class DropDownFacade<T> extends UIWrapper<SelectBox>{
-    SelectBox<T>mSelectBox;
+public class DropDownFacade extends UIWrapper<SelectBox>{
+    private final SelectBox mSelectBox;
+    private SelectBox.SelectBoxStyle mSelectBoxStyle;
+    private LuaFunction mCallBack;
+    private ChangeHandler mChangeHandler;
 
-    public DropDownFacade(BaseScreen screen,LuaTable luaTable,SelectBox.SelectBoxStyle style){
+    public DropDownFacade(BaseScreen screen,LuaTable luaTable){
         super(screen,luaTable);
-        mSelectBox=new SelectBox<T>(style);
+        mChangeHandler=new ChangeHandler();
+        mSelectBoxStyle=new SelectBox.SelectBoxStyle();
+        mSelectBox=new SelectBox(mSelectBoxStyle);
     }
     @Override
-    void setPosition(float x, float y) {
+    public void setPosition(float x, float y) {
         mSelectBox.setPosition(x, y);
+    }
+
+    @ExposeToLua
+    public void setOnChange(LuaFunction callBack) {
+        mCallBack = callBack;
+        if(mCallBack != null) {
+            mSelectBox.addListener(mChangeHandler);
+        }else {
+            mSelectBox.removeListener(mChangeHandler);
+        }
+
     }
 
     @Override
@@ -25,21 +44,33 @@ public class DropDownFacade<T> extends UIWrapper<SelectBox>{
 
     @Override
     public void dispose() {
-        mSelectBox=null;
+
     }
 
     @ExposeToLua
-    public T getSelected(){
-        return mSelectBox.getSelected();
+    public Actor getSelected(){
+        if(mSelectBox.getSelected() instanceof Actor)
+            return (Actor)mSelectBox.getSelected();
+        else
+            return null;
     }
 
     @ExposeToLua
-    public void setSelected(T item){
+    public void setSelected(Actor item){
         mSelectBox.setSelected(item);
     }
 
     @ExposeToLua
-    public List<T> getList(){
-        return mSelectBox.getList();
+    public Array getItems(){
+        return mSelectBox.getItems();
+    }
+
+    private class ChangeHandler extends ChangeListener {
+        @Override
+        public void changed(ChangeListener.ChangeEvent event, Actor actor)  {
+            if(mCallBack != null) {
+                mCallBack.call();
+            }
+        }
     }
 }
--- core/src/com/tsuru2d/engine/uiapi/ImageButtonFacade.java
+++ core/src/com/tsuru2d/engine/uiapi/ImageButtonFacade.java
@@ -1,38 +1,28 @@
 package com.tsuru2d.engine.uiapi;
 
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.tsuru2d.engine.BaseScreen;
-import com.tsuru2d.engine.loader.AssetID;
-import com.tsuru2d.engine.loader.ManagedAsset;
 import com.tsuru2d.engine.lua.ExposeToLua;
 import org.luaj.vm2.LuaFunction;
 import org.luaj.vm2.LuaTable;
 
 public class ImageButtonFacade extends ButtonSuper {
-    private ManagedAsset<String> mText;
     private ClickHandler mClickHandler;
     private LuaFunction mCallBack;
-    private TextButton.TextButtonStyle mTextButtonStyle;
-    private BitmapFont mFont;
     private final ImageButton mImageButton;
-    public ImageButtonFacade(BaseScreen screen, LuaTable luaTable,ImageButton.ImageButtonStyle style){
+    private ImageButton.ImageButtonStyle mImageButtonStyle;
+    public ImageButtonFacade(BaseScreen screen, LuaTable luaTable){
         super(screen, luaTable);
-        mFont = new BitmapFont();
-        mTextButtonStyle = new TextButton.TextButtonStyle();
-        mTextButtonStyle.font = mFont;
         mClickHandler = new ClickHandler();
-        mImageButton=new ImageButton(style);
+        mImageButtonStyle=new ImageButton.ImageButtonStyle();
+        mImageButton=new ImageButton(mImageButtonStyle);
     }
 
     @Override
     public void dispose(){
-        if (mText != null) {
-            mScreen.getAssetLoader().freeAsset(mText);
-        }
+
     }
 
     @Override
@@ -44,11 +34,6 @@ public class ImageButtonFacade extends ButtonSuper {
         mImageButton.setPosition(x,y);
     }
 
-    @ExposeToLua
-    public void setText(AssetID text) {
-        dispose();
-        mText = mScreen.getAssetLoader().getText(text);
-    }
 
     @ExposeToLua
     public void setOnClick(LuaFunction callBack) {
