510acfcb57600ad66b3172ecb9b70858ab32780c
Hertin <hertin@outlook.com>
2015-10-17 13:44:23 -0500

    GameActor, setTexture and draw

--- core/src/com/tsuru2d/engine/gameapi/GameActor.java
+++ core/src/com/tsuru2d/engine/gameapi/GameActor.java
@@ -1,7 +1,19 @@
 package com.tsuru2d.engine.gameapi;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class GameActor extends Actor {
+    private Texture mTexture;
 
+    public void setTexture(Texture texture) {
+        mTexture=texture;
+    }
+
+    @Override
+    public void draw (Batch batch, float parentAlpha) {
+        batch.draw(
+            mTexture,getX(),getY(),getOriginX(),getOriginY(),getHeight(),getWidth(),getScaleX(),getScaleY(),getRotation(),0,0,mTexture.getWidth(),mTexture.getHeight(),false,false);
+    }
 }
