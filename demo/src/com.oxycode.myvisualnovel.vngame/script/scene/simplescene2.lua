3748820a85a27531bbcaf6b1a953b82d98c27673
Hertin <hertin@outlook.com>
2015-10-26 07:49:46 -0500

    add some frames and changed the text. Also a small demonstration on how to add button and  characters.(I commented out the demon part. You can add it back when doing demon.)

--- demo/src/com.oxycode.myvisualnovel.vngame/script/scene/simplescene2.lua
+++ demo/src/com.oxycode.myvisualnovel.vngame/script/scene/simplescene2.lua
@@ -8,6 +8,9 @@ return function(scene)
             x = -400,
             y = 50
         })
+
+        -- state.linux=frame:create(R.object.linux, {x=-400, y = 50,alpha=0})
+
         frame:background(R.image.bg2)
     end)
     scene:frame("f1", function(frame, state)
@@ -25,7 +28,55 @@ return function(scene)
         frame:transform(state.bob, {
             x = {value = 400, duration = 0.5, interpolation = "pow2"}
         })
-        frame:text(R.text.simplescene1.isnt_that_cool)
+        frame:text(R.text.simplescene1.greet_Alice)
     end)
+    scene:frame("f3", function(frame, state)
+        frame:character(R.object.alice)
+        frame:transform(state.bob, {
+            x = {value =150, duration = 0.5, interpolation = "pow3"}
+        })
+        frame:transform(state.alice, {
+            x = {value =460, duration = 1, interpolation = "sineIn"}
+        })
+        frame:text(R.text.simplescene1.pretty)
+    end)
+    scene:frame("f4", function(frame, state)
+        frame:character(R.object.alice)
+        frame:transform(state.alice, {
+            x = {value =430, duration = 0.5, interpolation = "sineIn"},
+            scale= {value=1.1, duration=0.5,interpolation="pow4"}
+        })
+        frame:text(R.text.simplescene1.ugly)
+    end)
+    scene:frame("f4", function(frame, state)
+        frame:character(R.object.alice)
+        frame:transform(state.alice, {x =460, scale={value=1, duration=0.5,interpolation="pow4"}})
+
+        frame:text(R.text.simplescene1.drag)
+    end)
+    scene:frame("f5", function(frame, state)
+        frame:character(R.object.bob)
+        frame:transform(state.bob, {
+            x = {value =460, duration = 1.5, interpolation = "sineIn"}
+        })
+        frame:transform(state.alice, {
+            x = {value =800, duration = 1, interpolation = "sineIn"}
+        })
+        frame:text(R.text.simplescene1.copyright)
+    end)
+
+    -- scene:frame("f6", function(frame, state)
+    --     frame:character(R.object.bob)
+    --     frame:transform(state.bob, {
+    --         x = {value =-400, duration = 1.5, interpolation = "sineIn"}
+    --     })
+    --     frame:transform(state.alice, {
+    --         x = {value =2000, duration = 1, interpolation = "sineIn"}
+    --     })
+    --     frame:transform(state.linux,{x = {value =400, duration = 1, interpolation = "bounceIn"},
+    --                                 alpha={value=1, duration=1, interpolation="sineIn"}})
+    --     frame:text(R.text.simplescene1.linux)
+    -- end)
+
     return R.scene.simplescene1
 end
--- demo/src/com.oxycode.myvisualnovel.vngame/script/screen/mainmenu.lua
+++ demo/src/com.oxycode.myvisualnovel.vngame/script/screen/mainmenu.lua
@@ -4,28 +4,28 @@ function mainmenu:onCreate(screen)
     print("mainmenu::onCreate()")
     self.screen = screen
     -- screen:playMusic(R.music.bg1)
-    local startGameButton = screen:newTextButton()
-    startGameButton:setText(R.text.common.start_game)
+    local startGameButton = screen:newButton()
+    -- startGameButton:setText(R.text.common.start_game)
     startGameButton:setStyle({
-        up = R.image.play1,
-        down=R.image.play2,
-        hover=R.image.play2
+        up = R.image.button,
+        down=R.image.button_2,
+        hover=R.image.button_2
     })
     startGameButton:setOnClick(function()
         screen:setGameScreenNew(R.scene.simplescene1, {})
     end)
     screen:add(startGameButton):fillX()
 
-    local b2 = screen:newButton()
-    b2:setStyle({
-        up = R.image.b1,
-        down=R.image.b2,
-        hover=R.image.b3
-    })
-    b2:setOnClick(function()
-        print("ih")
-    end)
-    screen:add(b2):fillX()
+    -- local b2 = screen:newButton()
+    -- b2:setStyle({
+    --     up = R.image.windows,
+    --     down=R.image.windows2,
+    --     hover=R.image.windows2,
+    -- })
+    -- b2:setOnClick(function()
+    --     print("hi")
+    -- end)
+    -- screen:add(b2):fillX()
     
 end
 
