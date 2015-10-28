b62135a2582b97ebc467e5048672222f09cb3e92
Zonglin-Li6565 <lizonglin6565@163.com>
2015-10-27 19:23:40 -0500

    changed ActorFacade.java

--- core/src/com/tsuru2d/engine/uiapi/ActorFacade.java
+++ core/src/com/tsuru2d/engine/uiapi/ActorFacade.java
@@ -1,9 +1,13 @@
 package com.tsuru2d.engine.uiapi;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Disposable;
 import com.tsuru2d.engine.gameapi.BaseScreen;
+import com.tsuru2d.engine.loader.AssetID;
+import com.tsuru2d.engine.loader.ManagedAsset;
 import com.tsuru2d.engine.lua.ExposedJavaClass;
+import org.luaj.vm2.LuaTable;
 
 public abstract class ActorFacade<T extends Actor> extends ExposedJavaClass implements Disposable {
     protected final BaseScreen mScreen;
@@ -19,6 +23,15 @@ public abstract class ActorFacade<T extends Actor> extends ExposedJavaClass impl
 
     public T getActor() {
         return mActor;
+    }
+
+    protected ManagedAsset<Texture> getStyleAsset(LuaTable style, String key) {
+        if (style.get(key).isnil()) {
+            return null;
+        } else {
+            AssetID assetID = (AssetID)style.get(key).checkuserdata(AssetID.class);
+            return mScreen.getAssetLoader().getImage(assetID);
+        }
     }
 
     @Override
