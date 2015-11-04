a89f643442db3d3e23104e33032bfbc8595006eb
Zonglin-Li6565 <lizonglin6565@163.com>
2015-11-03 22:22:43 -0600

    Tab Container works. Let's eat some 金坷垃

--- core/src/com/tsuru2d/engine/uiapi/TabContainerFacade.java
+++ core/src/com/tsuru2d/engine/uiapi/TabContainerFacade.java
@@ -1,56 +1,117 @@
 package com.tsuru2d.engine.uiapi;
 
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Array;
 import com.tsuru2d.engine.gameapi.BaseScreen;
 import com.tsuru2d.engine.loader.AssetID;
+import com.tsuru2d.engine.loader.ManagedAsset;
 import com.tsuru2d.engine.lua.ExposeToLua;
-import com.tsuru2d.engine.lua.ExposedJavaClass;
 import org.luaj.vm2.LuaFunction;
 import org.luaj.vm2.LuaTable;
 import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.ZeroArgFunction;
 
-import java.util.HashMap;
-
-public class TabContainerFacade extends ActorFacade<Table>{
+public class TabContainerFacade extends ActorFacade<Table, TabContainerFacade.TabContainerStyle>{
     private TableFacade mTabs;
     private TableFacade mPanel;
-    private LuaTable mTabStyle;
+    private CallbackWrapper mFirstCallback;
+    private ManagedAsset<Texture> mBackground;
+    private AssetID mTabStyle;
+    private Array<TextButtonFacade> mTabArray;
 
-    public TabContainerFacade(BaseScreen screen) {
-        super(screen);
-        mTabs = new TableFacade(screen);
-        mPanel = new TableFacade(screen);
+    public TabContainerFacade(BaseScreen screen, AssetID styleID) {
+        super(screen, styleID);
+        mTabArray = new Array<TextButtonFacade>();
+    }
+
+    @Override
+    public Table createActor(TabContainerStyle tabContainerStyle) {
+        mTabs = new TableFacade(getScreen());
+        mPanel = new TableFacade(getScreen());
+        mTabs.initialize();
+        mPanel.initialize();
         Table table = new Table();
-        table.add(mTabs.getActor());
+        table.setDebug(true);
+        mTabs.getActor().setDebug(true);
+        mPanel.getActor().setDebug(true);
+        table.add(mTabs.getActor()).left();
         table.row();
-        table.add(mPanel.getActor());
-        setActor(table);
+        table.add(mPanel.getActor()).expand().fill();
+        table.setBackground(tabContainerStyle.background);
+        return table;
+    }
+
+    @Override
+    protected TabContainerStyle createStyle() {
+        TabContainerStyle style = new TabContainerStyle();
+        return style;
+    }
+
+    @Override
+    protected void populateStyle(TabContainerStyle style, LuaTable styleTable) {
+        mBackground = swapStyleImage(styleTable, "background", mBackground);
+        mTabStyle = (AssetID)styleTable.get("buttonStyle").checkuserdata(AssetID.class);
+        style.background = toDrawable(mBackground);
     }
 
     @ExposeToLua
-    public TableFacade getCleanPane() {
-        Table panel = mPanel.getActor();
-        for(Cell c : panel.getCells()) {
-            panel.removeActor(c.getActor());
-        }
-        return mPanel;
-    }
-
-    @ExposeToLua
-    public void setTabStyle(LuaTable tabStyle) {
-        mTabStyle = tabStyle;
+    public void replacePane(LuaValue table) {
+        TableFacade newTable = (TableFacade) table;
+        mPanel.getActor().clearChildren();
+        mPanel.add(newTable).expand().fill();
     }
 
     @ExposeToLua
     public void addTab(AssetID text, LuaFunction callBack) {
-        TextButtonFacade tab = new TextButtonFacade(mScreen);
-        tab.setText(text);
-        tab.setOnClick(callBack);
-        mTabs.add(tab);
+    //public void addTab(String text, LuaFunction callBack) {
+        TextButtonFacade tabFacade = new TextButtonFacade(getScreen(), mTabStyle);
+        mTabArray.add(tabFacade);
+        tabFacade.initialize();
+        tabFacade.setText(text, null);
+        CallbackWrapper callbackWrapper = new CallbackWrapper(callBack);
+        tabFacade.setClickListener(callbackWrapper);
+        mTabs.add(tabFacade).left();
+        if(mFirstCallback == null) {
+            mFirstCallback = callbackWrapper;
+            mFirstCallback.call();
+        }
     }
 
+    @Override
+    public void dispose() {
+        mBackground = freeAsset(mBackground);
+        for(int i = 0; i < mTabArray.size; i++) {
+            mTabArray.get(i).dispose();
+        }
+        mTabs.dispose();
+        mPanel.dispose();
+        super.dispose();
+    }
+
+    public class TabContainerStyle {
+        /**Optional*/
+        public Drawable background;
+        /**Optional*/
+        public AssetID tabStyle;
+        public TabContainerStyle() {
+        }
+    }
+
+    private class CallbackWrapper extends ZeroArgFunction {
+
+        private LuaFunction mCallback;
+        public CallbackWrapper(LuaFunction function) {
+            mCallback = function;
+        }
+        @Override
+        public LuaValue call() {
+            LuaValue table = mCallback.call();
+            replacePane(table);
+            return LuaValue.NIL;
+        }
+    }
 }
+
