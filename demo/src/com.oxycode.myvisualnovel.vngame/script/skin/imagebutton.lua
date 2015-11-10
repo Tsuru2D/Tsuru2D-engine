8c4d9c4c879d6b6583fd7d07f020f209836dd15b
Hertin <hertin@outlook.com>
2015-11-10 15:44:32 -0500

    add hovers and remake logout and resume and modified imagebutton.lua

--- demo/src/com.oxycode.myvisualnovel.vngame/script/skin/imagebutton.lua
+++ demo/src/com.oxycode.myvisualnovel.vngame/script/skin/imagebutton.lua
@@ -2,31 +2,31 @@ return {
     login = {
         up = R.image.imagebutton.login_up,
         down = R.image.imagebutton.login_down,
-        hover = R.image.imagebutton.login_down,
+        hover = R.image.imagebutton.login_hover,
     },
     logout = {
         up = R.image.imagebutton.logout_up,
         down = R.image.imagebutton.logout_down,
-        hover = R.image.imagebutton.logout_down,
+        hover = R.image.imagebutton.logout_hover,
     },
     start = {
         up = R.image.imagebutton.start_up,
         down = R.image.imagebutton.start_down,
-        hover = R.image.imagebutton.start_down
+        hover = R.image.imagebutton.start_hover
     },
     continue = {
         up = R.image.imagebutton.continue_up,
         down = R.image.imagebutton.continue_down,
-        hover = R.image.imagebutton.continue_down
+        hover = R.image.imagebutton.continue_hover
     },
     settings = {
         up = R.image.imagebutton.settings_up,
         down = R.image.imagebutton.settings_down,
-        hover = R.image.imagebutton.settings_down
+        hover = R.image.imagebutton.settings_hover
     },
     album = {
         up = R.image.imagebutton.album_up,
         down = R.image.imagebutton.album_down,
-        hover = R.image.imagebutton.album_down
+        hover = R.image.imagebutton.album_hover
     }
 }
