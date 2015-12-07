4b5545aa31f29ca9c7e556678ef6d705b846368c
NeoWu1216 <neo1216.wu@gmail.com>
2015-12-07 13:36:59 -0600

    Design basic components

--- desktop/src/com/tsuru2d/engine/desktop/FileBroswer.java
+++ desktop/src/com/tsuru2d/engine/desktop/FileBroswer.java
@@ -5,9 +5,13 @@ import javax.swing.*;
 public class FileBroswer {
     private JTabbedPane mTabbedPane1;
     private JPanel mPanel1;
+    private JTable mTable2;
+    private JButton mDownloadButton;
     private JTable mTable1;
+    private JButton mOpenButton;
+    private JButton mTestButton;
+    private JButton mUploadButton;
     private JButton mButton1;
-    private JButton mButton2;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
